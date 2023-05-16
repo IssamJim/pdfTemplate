13e64b29c3115ca37b5213179720b07a26cc756e
Issam Silverio Jimenez Ortega <issamjimenez.98@gmail.com>
2023-05-15 17:10:06 -0700

    Lineas

--- main.py
+++ main.py
@@ -12,7 +12,11 @@ for index, row in df.iterrows():
     pdf.set_font(family="Times", style="B", size=24 )
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
-    pdf.line(10, 21, 200, 21)
+
+    #Lineas
+    rango = range(20, 298, 10)
+    for i in rango:
+        pdf.line(10, i, 200, i)
 
     #Footer
 
@@ -23,7 +27,9 @@ for index, row in df.iterrows():
 
     for i in range(row["Pages"] - 1):
         pdf.add_page()
-
+        #Lineas
+        for i in rango:
+            pdf.line(10, i, 200, i)
         #Footer
 
         pdf.ln(277)
---
+++ pruebta.py
@@ -0,0 +1,4 @@
+rango = range(31, 255, 10)
+
+for i in rango:
+    print(i)
